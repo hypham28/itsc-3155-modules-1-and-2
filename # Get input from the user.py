9df54ca42da97184b0c8f9137f2f7131fa3f6091
hypham28 <143453496+hypham28@users.noreply.github.com>
2023-09-04 00:03:07 -0400

    Adding exercise 2 script 1

---
+++ # Get input from the user.py
@@ -0,0 +1,8 @@
+# Get input from the user
+user_input = input("Enter a string: ")
+
+# Reverse the string using slicing
+reversed_string = user_input[::-1]
+
+# Print the reversed string
+print("Reversed string:", reversed_string)
